f084c78095a151a3eeaf710a06656bc08dbcc6e0
poflotski <pavlitskiy.egor@mail.ru>
2016-12-07 01:48:04 +0300

    1

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+        <link rel="stylesheet" href="css/libs/normolize.css">
+        <link rel="stylesheet" href="css/libs/bootstrap.css">
+        <link rel="stylesheet" href="css/build/style.css" type="text/css">
+    </head>
+    <body>
+    <div class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-5 col-xs-6">
+                    <img class="logo" src="images/logo.png"/>
+                </div>
+                <div class="col-lg-2 col-lg-offset-7 col-md-3 col-md-offset-6 col-sm-5 col-sm-offset-1 col-xs-6 ">
+                    <button type="button" class="btn btn-primary">Подписаться</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<div class="container">
+    <div class="row banner">
+        <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <img class="city" src="images/city.jpg">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 news">
+            <h2>Новости</h2>
+        </div>
+    </div>
+    <div class="row article">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 article_title" ><h3> Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи</h3>
+        </div>
+        <div class="col-lg-4 col-md-4 pic col-sm-12">
+            <img class="art" src="images/art.jpg"/>
+        </div>
+            <div class="col-lg-8 col-md-8 col-sm-12 text">
+                OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turns
+                a regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+                receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+                insurance OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turnsa regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+                receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+                insurance companies. OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turns
+                a regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+                receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+                insurance companies.
+            </div>
+        <div class="date col-lg-12 col-md-12 col-sm-12 col-xs-12">дд/мм/гг</div>
+    </div>
+    <div class="row article">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 article_title" ><h3> Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи</h3>
+        </div>
+        <div class="col-lg-4 col-md-4 pic col-sm-12">
+            <img class="art" src="images/art.jpg"/>
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-12 text">
+            OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turns
+            a regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+            receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+            insurance OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turnsa regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+            receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+            insurance companies. OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turns
+            a regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+            receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+            insurance companies.
+        </div>
+        <div class="date col-lg-12 col-md-12 col-sm-12 col-xs-12">дд/мм/гг</div>
+    </div>
+    <div class="row article">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 article_title" ><h3> Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи Название статьи</h3>
+        </div>
+        <div class="col-lg-4 col-md-4 pic col-sm-12">
+            <img class="art" src="images/art.jpg"/>
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-12 text">
+            OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turns
+            a regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+            receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+            insurance OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turnsa regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+            receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+            insurance companies. OBD-II Scanner is a multifunctional in-vehicle diagnostic and monitoring device running Android OS. It turns
+            a regular car into a smart car and allows you to take care of your relatives, analyze driver behavior,
+            receive emergency notifications, protect your car from theft, check vehicle data and save money on fuel and
+            insurance companies.
+        </div>
+        <div class="date col-lg-12 col-md-12 col-sm-12 col-xs-12">дд/мм/гг</div>
+    </div>
+</div>
+    <div class="footer">
+        <div class="container aboutUs">
+            <div class="row ">
+                <div class="col-lg-12 " >
+                    <div class="aboutUs">
+                        Canada IT Advance and Deliverance Corp. provides connected car, safety and security solutions worldwide. Our
+                        company provides services for development and distribution of telematic solutions using the Internet and
+                        software running on servers, as well as on wearable devices: smart phones, tablets, smart watches and other
+                        gadgets.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
---
+++ php/subscribe.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/libs/normolize.css">
+    <link rel="stylesheet" href="css/libs/bootstrap.css">
+    <link rel="stylesheet" href="css/build/style.css" type="text/css">
+</head>
+<body>
+<div class="header container ">
+    <div class="row ">
+        <div class="col-lg-3 col-lg-offset-3 col-md-4 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-6">
+            <img class="logo" src="images/logo.png"/>
+        </div>
+        <div class="col-lg-3 col-lg-offset-2 col-md-2 col-md-offset-4 col-sm-3 col-sm-offset-3 col-xs-6 ">
+            <button type="button" class="btn btn-primary">Подписаться</button>
+        </div>
+    </div>
+</div>
+<div class="content container">
+    <div class="row banner">
+        <div class=" col-lg-8 col-lg-offset-2 col-md-12">
+            <img class="city" src="images/city.jpg">
+        </div>
+    </div>
+    <div class=" row">
+        <div class="news col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 ">
+            <h2>Новости</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 " >
+            <div class="aboutUs footer">
+                Canada IT Advance and Deliverance Corp. provides connected car, safety and security solutions worldwide. Our
+                company provides services for development and distribution of telematic solutions using the Internet and
+                software running on servers, as well as on wearable devices: smart phones, tablets, smart watches and other
+                gadgets!
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
